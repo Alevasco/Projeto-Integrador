f1f28cc86c2aeaebebb9f43107eff84a82e55be6
Alevasco <ale_vasconcelos96@hotmail.com>
2022-04-06 09:00:44 -0300

    Tela de login

--- app/src/main/java/com/example/ajuda/FormLogin.kt
+++ app/src/main/java/com/example/ajuda/FormLogin.kt
@@ -2,13 +2,13 @@ package com.example.ajuda
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import dagger.hilt.android.AndroidEntryPoint
 
-@AndroidEntryPoint
-class MainActivity : AppCompatActivity() {
+class FormLogin : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_form_login)
+
+        supportActionBar?.hide()
 
     }
 }
