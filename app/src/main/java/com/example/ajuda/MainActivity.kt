01a0c2cbe096aeb6a5e3df7691a038dc180755dc
DuduFN <dudu.fn1364@gmail.com>
2022-04-06 18:53:48 -0300

    splash screen

--- app/src/main/java/com/example/ajuda/MainActivity.kt
+++ app/src/main/java/com/example/ajuda/MainActivity.kt
@@ -7,7 +7,9 @@ import dagger.hilt.android.AndroidEntryPoint
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
+        Thread.sleep(500)
         super.onCreate(savedInstanceState)
+        setTheme(R.style.Theme_Ajuda)
         setContentView(R.layout.activity_main)
 
     }
